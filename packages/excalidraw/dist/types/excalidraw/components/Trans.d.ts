9f6e7ab212c7c78a8ba675cb7fabd171a82b1821
vitallygolovanov <89348452+vitallygolovanov@users.noreply.github.com>
2026-06-18 17:23:32 +0300

    build: regenerate dist types (shouldSuppressDrop prop, Trans FragmentProps)

--- packages/excalidraw/dist/types/excalidraw/components/Trans.d.ts
+++ packages/excalidraw/dist/types/excalidraw/components/Trans.d.ts
@@ -3,7 +3,5 @@ import type { TranslationKeys } from "../i18n";
 declare const Trans: ({ i18nKey, children, ...props }: {
     [key: string]: React.ReactNode | ((el: React.ReactNode) => React.ReactNode);
     i18nKey: TranslationKeys;
-}) => React.FunctionComponentElement<{
-    children?: React.ReactNode;
-}>;
+}) => React.FunctionComponentElement<React.FragmentProps>;
 export default Trans;
